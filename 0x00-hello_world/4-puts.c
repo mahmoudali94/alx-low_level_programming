a8155037ce987b8edc71aa35df7ec66e8bfcefa6
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-16 18:50:49 -0700

    solve task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main is the entry point to  print the phrase "Programming is like building a multilingual puzzle
+ * main is the entry point to  prpgram
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
