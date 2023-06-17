29db114bce37e49bef4b617d77298008e7ecb6f8
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-16 18:46:27 -0700

    solve task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-/* Print using puts function */
-int main ()
+/* Print using puts function
+ * main function is the entry point
+ */
+int main(void)
 {
 	/*Print string */
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle ");
 	return (0);
 }
