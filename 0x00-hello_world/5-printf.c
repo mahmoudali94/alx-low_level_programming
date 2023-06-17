bbe20ef6b23ba77596e3cab72c59d80ca86e5565
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-16 18:56:19 -0700

    solve task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to print "Programming is like..
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
