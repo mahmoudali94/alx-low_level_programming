bc7c3f8f84fedf6029ae2b88980bfa9be91e87a8
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-20 16:35:45 -0700

    printf function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,16 @@
  * return alaways 0
  */
 int main(void)
-{
-	printf("_putchar");
-	return(0);
+{	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return(0);
 }
