7096b2374a2909e90206faf2cebe32902d0e5dc0
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-20 17:18:08 -0700

    print alphapet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet -  prints a to z, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
