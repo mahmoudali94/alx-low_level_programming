80db88ffc479f4cee84ef77bc4b654ccae979787
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-22 18:15:26 -0700

    fix error

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
+	int month;
 	int day;
 	int year;
-	int month;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 02;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
