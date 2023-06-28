238cd0778106f89adce73e958f879a8a0b551201
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-06-27 19:34:33 -0700

    advance task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,11 @@
  */
 int _atoi(char *s)
 {
-	int i, g, h, len, f, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
-	g = 0;
-	h = 0;
+	d = 0;
+	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
@@ -28,9 +28,9 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (g % 2)
+			if (d % 2)
 				digit = -digit;
-			h = h *10 + digit;
+			n = n *10 + digit;
 			f = 1;
 			if (s[i + 1]<'0' || s[i + 1] > '9')
 				break;
