107af00ada9bc1d4241f0871d97b7ce309c6af03
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-07-04 01:21:52 -0700

    Strings and Arrys

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,18 +6,19 @@
  *
  * Return: char with result
  */
-char * _strchr(char * s, char c)
+char *_strchr(char *s, char c)
 {
-
-  if ( * s == '\0')
-    return (s);
-  while ( * s) {
-    if ( * s == c) {
-      return (s);
-    }
-    s++;
-  }
-  if (c == '\0')
-    return (s);
-  return ('\0');
+	if (*s == '\0')
+		return (s);
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return ('\0');
 }
