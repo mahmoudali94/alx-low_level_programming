f46a02340b8ea5e33cf1c37bbf01d8944ee6a783
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-07-07 19:28:26 -0700

    print count of parameters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of paramters
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
