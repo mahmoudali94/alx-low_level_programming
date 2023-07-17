abf237061bcc98b69f4046c782d6c5ce637704a7
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-07-17 15:18:27 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
